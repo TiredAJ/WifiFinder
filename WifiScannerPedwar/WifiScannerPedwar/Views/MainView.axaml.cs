4f9191f2920781c36c7a2b4fc4a71223640db985
AJ <aletha.e.jones@gmail.com>
2023-10-02 14:52:33 +0100

    testing

--- WifiScannerPedwar/WifiScannerPedwar/Views/MainView.axaml.cs
+++ WifiScannerPedwar/WifiScannerPedwar/Views/MainView.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Threading;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Reflection.PortableExecutable;
+using WifiScannerLib;
 using SDD = System.Diagnostics.Debug;
 
 namespace WifiScannerPedwar.Views;
@@ -20,6 +21,26 @@ public partial class MainView : UserControl
         BTN_Snapshot.ClickMode = ClickMode.Press;
 
         BTN_Snapshot.Click += Snapshot_Click;
+
+        DGRD_APData.SelectionChanged += SelectionChanged;
+    }
+
+    private void SelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        //int Index = DGRD_APData.SelectedIndex;
+        WifiInfoItem? WII = DGRD_APData.SelectedItem as WifiInfoItem;
+
+        if (WII != null)
+        {
+            Dispatcher.UIThread.InvokeAsync(() =>
+            { 
+                TXT_BSSID.Text = WII.BSSID;
+                TXT_SSID.Text = WII.SSID;
+                TXT_Strength.Text = WII.RSSI;
+            });
+        }
+
+        SDD.WriteLine($"Selected item: {DGRD_APData.SelectedItem}");
     }
 
     private void Snapshot_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
---
+++ WifiScannerPedwar/WifiScannerPedwar/Views/WifiListView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WifiScannerPedwar;
+
+public partial class WifiListView : UserControl
+{
+    public WifiListView()
+    {
+        InitializeComponent();
+    }
+}
