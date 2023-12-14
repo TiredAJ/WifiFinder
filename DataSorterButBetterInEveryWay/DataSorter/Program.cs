7e93cb868929a83ef6759dbc601f712b408bd52c
AJ <aletha.e.jones@gmail.com>
2023-12-14 01:42:32 +0000

    maybe getting somewhere, but also it'a 1:40 am. Although it's probably 6pm somewhere
    Turns out it's nearly 6pm in San Francisco

--- DataSorterButBetterInEveryWay/DataSorter/Program.cs
+++ DataSorterButBetterInEveryWay/DataSorter/Program.cs
@@ -7,7 +7,8 @@ namespace DataSorter
 {
     class Program
     {
-        static List<List<SnapshotData>> Data = new List<List<SnapshotData>>();
+        static List<List<SnapshotData>> InitData = new List<List<SnapshotData>>();
+        static Dictionary<string, FlattenedData> Data = new Dictionary<string, FlattenedData>();
 
         static void Main(string[] args)
         {
@@ -35,14 +36,86 @@ namespace DataSorter
                     foreach (var JN in JNode.AsArray())
                     { TempList.Add(new SnapshotData(JN)); }
 
-                    Data.Add(TempList);
+                    InitData.Add(TempList);
                     TempList.Clear();
                 }
             }
 
-            Console.WriteLine(Data.Count());
+            Console.WriteLine(InitData.Count());
         }
 
+        static void FlattenData()
+        {
+            Dictionary<string, List<FlattenedData>> Intermediate = new Dictionary<string, List<FlattenedData>>();
 
+            //you fucking idiot, it needs to be flattened to per node,
+            //not per AP
+
+            foreach (var ID in InitData)
+            {
+                foreach (var Snap in ID)
+                {
+                    foreach (var KVP in Snap.Data)
+                    {
+                        if (Intermediate.ContainsKey(KVP.Key))
+                        { Intermediate[KVP.Key].Add(new FlattenedData(KVP.Value)); }
+                        else
+                        {
+                            Intermediate.Add(KVP.Key, new List<FlattenedData>()
+                                { new FlattenedData(KVP.Value) });
+                        }
+                    }
+                }
+            }
+
+            foreach (var I in Intermediate)
+            { Data.Add(I.Key, I.Value.Average()); }
+        }
+
+        private void SaveData()
+        {
+            string Temp = Path.Combine(Environment.CurrentDirectory, "Summed");
+
+            using (StreamWriter Writer = new StreamWriter())
+            {
+
+            }
+        }
+    }
+
+    public struct FlattenedData
+    {
+        public int RSSI { get; set; }
+        public double Distance { get; set; }
+
+        public FlattenedData(int _RSSI, double _Distance)
+        {
+            RSSI = _RSSI;
+            Distance = _Distance;
+        }
+
+        public FlattenedData(WifiInfoItem _WII)
+        {
+            RSSI = _WII._RSSI;
+            Distance = _WII._Distance;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static FlattenedData Average(this List<FlattenedData> _Input)
+        {
+            FlattenedData Temp = new FlattenedData();
+
+            Temp.RSSI = (int)_Input
+                            .Select(X => X.RSSI)
+                            .Average();
+
+            Temp.Distance = _Input
+                            .Select(X => X.Distance)
+                            .Average();
+
+            return Temp;
+        }
     }
 }
